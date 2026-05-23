9709590cba6768338676136cb588aff22c7b71a1
Abhayraj Singh Mandloi <professionalabhayraj@gmail.com>
2026-05-23 17:39:34 +0530

    Fix duplicate api path

--- amazon-frontend/src/Axios.js
+++ amazon-frontend/src/Axios.js
@@ -1,7 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
-const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
-})
-
-export default instance
+export default axios.create({
+    baseURL: process.env.REACT_APP_API_URL.replace('/api', '')
+});
