42ab6e2ad3788849a5bf3a5605b421d510571d4c
Abhayraj Singh Mandloi <professionalabhayraj@gmail.com>
2026-05-23 17:45:08 +0530

    Fix duplicate api path issue

--- amazon-frontend/src/actions/ProdcutActions.js
+++ amazon-frontend/src/actions/ProdcutActions.js
@@ -1,27 +1,27 @@
-import axios from "../Axios";
-import { 
-    PRODUCT_LIST_REQUEST, 
-    PRODUCT_LIST_FAIL, 
+import { productAPI } from "../services/api";
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_FAIL,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_DETAILS_REQUEST, 
-    PRODUCT_DETAILS_FAIL, 
-    PRODUCT_DETAILS_SUCCESS 
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_SUCCESS
 
 } from "../constants/ProductConstants";
 
-export const listProducts = () => async (dispatch) =>{
+export const listProducts = () => async (dispatch) => {
     dispatch({
-        type: PRODUCT_LIST_REQUEST 
+        type: PRODUCT_LIST_REQUEST
     });
 
-    try{
-        const {data} = await axios.get("/api/products");
+    try {
+        const { data } = await productAPI.getAllProducts();
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
             payload: data.products
         })
     }
-    catch(error){
+    catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
             payload: error.message
@@ -31,26 +31,26 @@ export const listProducts = () => async (dispatch) =>{
 
 
 
-export const detailsProduct = (productID) => async (dispatch) =>{
+export const detailsProduct = (productID) => async (dispatch) => {
     dispatch({
         type: PRODUCT_DETAILS_REQUEST,
         payload: productID
     });
 
-    try{
-        const {data} = await axios.get(`/api/products/${productID}`);
-   
+    try {
+        const { data } = await productAPI.getProductById(productID);
+
         dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
             payload: data
         })
     }
-    catch(error){
+    catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
-            payload: error.response && error.response.data.mesaage
-            ? error.response.data.message
-            : error.message,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
         });
     }
 };
